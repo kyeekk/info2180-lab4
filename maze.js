64b157cf96a3f413c6e24227973c60d0eeed1e2f
kyeekk <kaylaeffs1998@gmail.com>
2018-10-19 13:51:43 -0500

    exrcise 2 finished

--- maze.js
+++ maze.js
@@ -2,10 +2,18 @@
 
 window.onload = () =>{
     let boundary = document.querySelectorAll(".boundary");
-    let bound1 = document.getElementById("#boundary1")
-    let start = document.getElementById("#start");
-    let maze  = document.getElementById("#maze");
-    let end = document.getElementById("#end");
-}
+    let bound1 = document.getElementById("boundary1")
+    let start = document.getElementById("start");
+    let maze  = document.getElementById("maze");
+    let end = document.getElementById("end");
 
-function 
+    bound1.addEventListener("mouseover", youLose)
+
+    function youLose (){
+        this.classList.add("youlose");
+    }
+
+    boundary.forEach( b => {b.addEventListener("mouseover", youLose)});
+    
+}
+ 
