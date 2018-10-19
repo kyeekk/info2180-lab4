a40032c5f013777a88dd79b777abdd7de39b148d
kyeekk <kaylaeffs1998@gmail.com>
2018-10-19 13:44:15 -0500

    adding exercise 1

---
+++ maze.js
@@ -0,0 +1,11 @@
+
+
+window.onload = () =>{
+    let boundary = document.querySelectorAll(".boundary");
+    let bound1 = document.getElementById("#boundary1")
+    let start = document.getElementById("#start");
+    let maze  = document.getElementById("#maze");
+    let end = document.getElementById("#end");
+}
+
+function 
